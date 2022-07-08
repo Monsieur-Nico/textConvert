2ef41ac096dbcedd59ce12f3ffdb96a0d23dc333
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-07-08 13:53:17 -0400

    Implement PascalCase function

---
+++ src/Conventions/pascalCase.ts
@@ -0,0 +1,22 @@
+/**
+ *
+ * @param text input text to convert to PascalCase
+ */
+
+export function pascalCase(text: string): string {
+  // Make sure there's an input
+  if (!text) return "Please provide a valid input text";
+
+  // define the regex pattern
+  const wordsArray: string[] = text.split(/[-_ ]/g);
+
+  // convert the words to camelCase
+  const pCaseArray: string[] = wordsArray.map((word: string) => {
+    word = word.charAt(0).toUpperCase();
+    return word;
+  });
+
+  // Join the words and return them
+  const result: string = pCaseArray.join("");
+  return result;
+}
