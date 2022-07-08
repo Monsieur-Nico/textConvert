0d953b1675398b84a88217fb9c67aa996eb6ab6d
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-07-08 15:26:55 -0400

    Convert the reges to an object

---
+++ src/assets/regex.ts
@@ -0,0 +1,15 @@
+/**
+ * Make a regex list to split words based on these patterns
+ */
+
+const regex = {
+  name: "Regex List",
+  description: "Regex patterns list to be used in functions",
+  values: {
+    upperCase: /[A-Z]/g,
+    lowerCase: /[a-z]/g,
+    spaceDash: /[-_ ]/g,
+  },
+};
+
+export { regex };
