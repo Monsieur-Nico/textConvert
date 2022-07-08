1fea7874fca6ae553814512fb1caee85bbc85d2e
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-07-07 22:04:40 -0400

    Implement camelCase convention

---
+++ src/Conventions/camelCase.ts
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param text input text to convert to CamelCase
+ */
+const camelCase = (text: string): string => {
+  // Make sure there's an input
+  if (!text) return "Please provide a valid input text";
+
+  // define the regex pattern
+  const wordsArray: string[] = text.split(/[-_ ]/g);
+  // Get the first word out of the array
+  const firstWord = wordsArray.shift();
+
+  // convert the words to camelCase
+  const cCaseArray: string[] = wordsArray.map((word: string) => {
+    word = word.charAt(0).toUpperCase() + word.slice(1);
+    return word;
+  });
+
+  // Join the words and return them
+  const result: string = firstWord + cCaseArray.join("");
+  return result;
+};
