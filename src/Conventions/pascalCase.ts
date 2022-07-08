731ef6566e2fcd299f57be164130a239baeec25d
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-07-08 17:01:42 -0400

    Update pascalCase function

--- src/Conventions/pascalCase.ts
+++ src/Conventions/pascalCase.ts
@@ -11,7 +11,7 @@ export function pascalCase(text: string): string {
   // Make sure there's an input
   if (!text) return "Please provide a valid input text";
 
-  // define the regex pattern
+  // Make an array of words after splitting them
   const wordsArray: string[] = text.split(values.spaceDash);
 
   // convert the words to camelCase
