2c839ef220a4edc3db4e5a472d3a183b45ea928c
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-07-10 19:13:56 -0400

    Write numbers in words

---
+++ src/assets/numbers.ts
@@ -0,0 +1,38 @@
+const numbers = {
+  dg: {
+    one: "one",
+    two: "two",
+    three: "three",
+    four: "four",
+    five: "five",
+    six: "six",
+    seven: "seven",
+    eight: "eight",
+    nine: "nine",
+  },
+  tn: {
+    ten: "ten",
+    eleven: "eleven",
+    twelve: "twelve",
+    thirteen: "thirteen",
+    fourteen: "fourteen",
+    fifteen: "fifteen",
+    sixteen: "sixteen",
+    seventeen: "seventeen",
+    eighteen: "eighteen",
+    nineteen: "nineteen",
+  },
+  tw: {
+    twenty: "twenty",
+    thirty: "thirty",
+    fourty: "fourty",
+    fifty: "fifty",
+    sixty: "sixty",
+    seventy: "seventy",
+    eighty: "eighty",
+    ninety: "ninety",
+  },
+  th: [],
+};
+
+export { numbers };
