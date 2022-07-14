2df82e5c327eb42396f648878d113bd97eb8cc67
Nicolas Alkhoury <74289847+Monsieur-Nico@users.noreply.github.com>
2022-07-13 23:05:21 -0400

    Export numbersToWords function

--- src/textConvert.ts
+++ src/textConvert.ts
@@ -7,5 +7,8 @@ import {
 
 import { clear } from "./Text/clear";
 
+import { numbersToWords } from "./Numbers/numbersToWords";
+
 export { camelCase, pascalCase, snakeCase, kebabCase };
 export { clear };
+export { numbersToWords };
