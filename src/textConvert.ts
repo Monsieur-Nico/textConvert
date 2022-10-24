99ca95b89c3c29c8351e11b407b282221954fbc0
Nicolas Alkhoury <74289847+Monsieur-Nico@users.noreply.github.com>
2022-10-24 10:48:50 -0400

    import and export new function

--- src/textConvert.ts
+++ src/textConvert.ts
@@ -7,10 +7,11 @@ import {
 
 import { clear } from "./Text/clear";
 import { reverse } from "./Text/reverse";
+import count from "./Text/count";
 
 import { numbersToWords } from "./Numbers/numbersToWords";
 
 export { camelCase, pascalCase, snakeCase, kebabCase };
-export { clear };
+export { clear, count };
 export { reverse };
 export { numbersToWords };
