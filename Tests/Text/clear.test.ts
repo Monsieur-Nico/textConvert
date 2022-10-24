1f421906138ae04361fc17878663249897ac526e
Nicolas Alkhoury <74289847+Monsieur-Nico@users.noreply.github.com>
2022-10-23 21:54:07 -0400

    Modify Clear to only return a string

--- Tests/Text/clear.test.ts
+++ Tests/Text/clear.test.ts
@@ -3,23 +3,11 @@ import { it } from "mocha";
 import { clear } from "../../src/textConvert";
 
 describe("#clear", () => {
-  it("should return ['hello', 'world'] for 'hello-world'", () => {
-    assert.deepStrictEqual(clear("hello-world"), ["hello", "world"]);
-  });
-  it("should return ['hello', 'world'] for 'Hello-world'", () => {
-    assert.deepStrictEqual(clear("Hello-world"), ["hello", "world"]);
-  });
-  it("should return ['hello', 'world'] for 'hello_world'", () => {
-    assert.deepStrictEqual(clear("hello_world"), ["hello", "world"]);
-  });
-  it("should return ['hello', 'world'] for 'hello world'", () => {
-    assert.deepStrictEqual(clear("hello world"), ["hello", "world"]);
-  });
   it("should return 'hello world' for 'Hello,world'", () => {
-    assert.strictEqual(clear("Hello,world", false), "hello world");
+    assert.strictEqual(clear("Hello,world"), "hello world");
   });
   it("should return 'hello world' for 'Hello world'", () => {
-    assert.strictEqual(clear("Hello world", false), "hello world");
+    assert.strictEqual(clear("Hello world"), "hello world");
   });
   it("should return 'Please provide a valid input text' for empty input", () => {
     assert.strictEqual(clear(""), "Please provide a valid input text");
--- src/Text/clear.ts
+++ src/Text/clear.ts
@@ -6,22 +6,12 @@ const { values } = regex;
  * Clear punctuations from a string and replaces it with a whitespace character or returns an array of strings.
  *
  * @param text String input to clear from punctuation.
- * @param arrayOutput (Optional), boolean value to select whether to return an array of strings or a single string.
- * Default value is true.
  * @returns Either a string or an array of strings cleared from punctuations based on the arguments passed.
  */
 
-export function clear(text: string, arrayOutput = true): string | string[] {
+export function clear(text: string): string {
   // Make sure there's an input
   if (!text) return "Please provide a valid input text";
-
-  // Check if the output should be an array of strings
-  if (arrayOutput) {
-    return text
-      .split(values.nonAlphaTest)
-      .filter((word: string) => word.length > 0)
-      .map((word: string) => word.trim().toLowerCase());
-  }
 
   // Create an array of words
   const wordsArray: string[] = text.split(values.punctuation);
